481dc85eee66ebd1ce720ab1a12cf767e3eeda71
IoanaSo <ioanasova2@gmail.com>
2025-07-22 12:16:39 +0200

    Add files via upload

---
+++ src/components/MiComponente.jsx
@@ -0,0 +1,8 @@
+
+export default function MiComponente() {
+  return (
+    <div className="p-4 bg-green-100 rounded">
+      <h2 className="text-xl font-semibold text-green-800">Soy un componente React</h2>
+    </div>
+  );
+}
