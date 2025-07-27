dae0614902b06928f0a199b982ca2c72438314aa
IoanaSo <ioanasova2@gmail.com>
2025-07-27 12:42:18 +0200

    Update spClient.ts

--- src/lib/spClient.ts
+++ src/lib/spClient.ts
@@ -5,3 +5,13 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
+// public/lib/spClient.js
+
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const supabase = createClient(
+  'https://uapfvdwbwfwhlwsqokbs.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhcGZ2ZHdid2Z3aGx3c3Fva2JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNzgyNTMsImV4cCI6MjA2ODc1NDI1M30.6MRmvrUNzP-sk-Msp7ckrEvRYh04vyeWIcQ5jI57Zpc'
+);
+
+
