81f7549ca8b31dc17d071eceacd577c3aa6493b0
IoanaSo <ioanasova2@gmail.com>
2025-07-28 19:50:20 +0200

    Update catalogScript.js

--- src/scripts/catalogScript.js
+++ src/scripts/catalogScript.js
@@ -4,7 +4,7 @@ import { supabase } from '../lib/spClient.js';
   import { createClient } from '@supabase/supabase-js';
   const supabase = createClient('https://uapfvdwbwfwhlwsqokbs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhcGZ2ZHdid2Z3aGx3c3Fva2JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNzgyNTMsImV4cCI6MjA2ODc1NDI1M30.6MRmvrUNzP-sk-Msp7ckrEvRYh04vyeWIcQ5jI57Zpc');
 
-  const { data, error } = await supabase.from('mi_tabla').select();
+  const { data, error } = await supabase.from('br1').select();
   console.log(data);
 
 
