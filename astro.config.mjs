4b36cf5af816691c3bd3cc3b6efb5ace82a18bd6
IoanaSo <ioanasova2@gmail.com>
2025-07-28 20:30:53 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,20 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
+//export default defineConfig({
+  //adapter: vercel(),
+  //base: '/IMDB_astro/', // Esto solo si tu repo/deploy está en esa subruta
+
+  //integrations: [react()],
+  //vite: {
+  //plugins: [tailwindcss()]
+  //}
+//});
+
+
 export default defineConfig({
-  adapter: vercel(),
-  base: '/IMDB_astro/', // Esto solo si tu repo/deploy está en esa subruta
-
-  integrations: [react()],
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://ioanaso.github.io',
+  base: '/IMDB_astro',
+  output: 'static',
 });
-
-
 
